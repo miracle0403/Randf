dd6ca2f9455bbe4d82f24557aa2eb36d6cfc8313
Miracle lawrence <lawrencemiracle72@gmail.com>
2025-03-06 19:20:00 +0100

    fill words nest

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Randf Construction' });
+  res.render('index', { title: 'Randf Hospital' });
 });
 
 module.exports = router;
